6176f3a845c9b5faa7c0a17553595575058a4e36
Ubuntu <ubuntu@ip-172-31-90-180.ec2.internal>
2019-04-09 22:12:08 +0000

    Add add_activation utility.

--- pollux/activations.py
+++ pollux/activations.py
@@ -1,4 +1,5 @@
 from tensorflow.keras import backend as K
+from tensorflow.keras.layers import Activation, PReLU, LeakyReLU
 from tensorflow.keras.utils import get_custom_objects
 
 def swish(x):
@@ -11,3 +12,13 @@ def softplus_eps(x):
     return K.softplus(x) + K.epsilon()
     
 get_custom_objects().update({'swish': swish, 'retanh': retanh, 'softplus_eps':softplus_eps})
+
+def add_activation(x, activation):
+    if activation in ['relu', 'selu', 'elu', 'sigmoid', 'hard_sigmoid', 'linear', 'tanh', 'softplus', 'softsign', 'softmax', 'swish', 'retanh', 'softplus_eps']:
+        return Activation(activation)(x)
+    elif activation == 'LeakyReLU':
+        return LeakyReLU()(x)
+    elif activation == 'PReLU':
+        return PReLU()(x)
+    else:
+        raise NotImplemented
